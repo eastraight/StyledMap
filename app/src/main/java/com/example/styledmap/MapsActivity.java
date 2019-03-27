032d2bd96bc792347549fed9e7975e5308b1153f
Eleanor <eleanor.straight@my.wheaton.edu>
2019-03-26 23:03:03 -0500

    Added a "You are here" marker on the user's location.
    Only works if Location permissions are already turned on (will not prompt the user / ask for permission on its own)

--- app/src/main/java/com/example/styledmap/MapsActivity.java
+++ app/src/main/java/com/example/styledmap/MapsActivity.java
@@ -1,7 +1,13 @@
 package com.example.styledmap;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Looper;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -10,7 +16,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.location.Location;
 
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -19,6 +29,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -29,6 +48,12 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private Button parkingToggle;
     private Button buildingToggle;
     private Button housingToggle;
+
+
+    /* Object used to receive location updates */
+    private FusedLocationProviderClient mFusedLocationClient;
+    /* Object that defines important parameters regarding location request. */
+    private LocationRequest locationRequest;
 
 
     @Override
@@ -69,7 +94,19 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 Toast.makeText(getApplicationContext(), "Housing is filtered", Toast.LENGTH_LONG).show();
             }
         });
-        }
+
+
+        //location stuff:
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(5000); // 5 second delay between each request
+        locationRequest.setFastestInterval(5000); // 5 seconds fastest time in between each request
+        locationRequest.setSmallestDisplacement(10); // 10 meters minimum displacement for new location request
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY); // enables GPS high accuracy location requests
+
+        sendUpdatedLocationMessage();
+    }
 
 
     @Override
@@ -124,12 +161,12 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
 
         // Add a marker in the CS lab and move the camera
-        LatLng meysci = new LatLng(41.869559, -88.096015);
-        mMap.addMarker(new MarkerOptions().position(meysci).title("Meyer Science Center"));
+        // LatLng meysci = new LatLng(41.869559, -88.096015);
+        // mMap.addMarker(new MarkerOptions().position(meysci).title("Meyer Science Center"));
 
         // Create a LatLngBounds that includes the Campus of Wheaton.
         LatLngBounds WHEATON = new LatLngBounds(
-                new LatLng(41.864417, -88.103536), new LatLng(41.873451, -88.088258));
+                new LatLng(41.865395, -88.103472), new LatLng(41.873451, -88.088258));
         // Constrain the camera target to the Wheaton.
         mMap.setLatLngBoundsForCameraTarget(WHEATON);
 
@@ -140,10 +177,52 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         // Set center point for the map at startup
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(WHEATON.getCenter(), 15.5f));
 
+        Log.d("ONMAP", "onmap is complete");
 
 
     }
 
+
+
+
+    /*
+        (This method from github user kaushikravikumar, RealtimeTaxiAndroidDemo project)
+
+        Checks user's location permission to see whether user has granted access to fine location and coarse location.
+        If not it will request these permissions.
+
+     */
+    public void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {//Can add more as per requirement
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    123);
+        }
+    }
+
+    /*
+    This method gets user's current location
+ */
+    private void sendUpdatedLocationMessage() {
+        Log.d("SEND", "sendUpdatedLocationMessage() in process");
+        mFusedLocationClient.requestLocationUpdates(locationRequest, new LocationCallback() {
+
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                Location location = locationResult.getLastLocation();
+
+                Log.d("ONLOCATIONRESULT", "reached onLocationResult");
+
+                // Add a marker on the user's current location
+                LatLng youAreHere = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(youAreHere).title("You are here"));
+
+            }
+        }, Looper.myLooper());
+    }
 
 
 }
