a853163e8ea5ea156db09aa55d658512dea67cf2
Marshall <Marshall.kirk@my.wheaton.edu>
2019-04-09 20:27:34 -0500

    Added SearchActivity java file for search bar

---
+++ app/src/main/java/com/example/styledmap/SearchActivity.java
@@ -0,0 +1,24 @@
+package com.example.styledmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.R.*;
+import android.util.Xml;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.*;
+import android.*;
+import android.widget.SearchView;
+
+public class SearchActivity extends AppCompatActivity {
+        public void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.Search);
+        }
+    }
+
