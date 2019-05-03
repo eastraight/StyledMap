ae611a49389f853b5ce0f598fcdd7374a1dd6f84
joshdemoss <josh.demoss@my.wheaton.edu>
2019-05-03 04:05:14 -0500

    Idk anymore

--- app/src/main/java/com/example/styledmap/OptionsActivity.java
+++ app/src/main/java/com/example/styledmap/OptionsActivity.java
@@ -3,8 +3,10 @@ package com.example.styledmap;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class OptionsActivity extends AppCompatActivity {
     private Button housesButton;
@@ -12,11 +14,18 @@ public class OptionsActivity extends AppCompatActivity {
     private Button upperButton;
     private Button apartButton;
     private Button backButton;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.housing_option_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Housing Options");
+
+
 
 
         housesButton = findViewById(R.id.housingButton);
